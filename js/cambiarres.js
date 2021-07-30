065fa25f34cba3e337f9a877177c89f035c605bf
OMEGeneralPico <estadistica.gpico@gmail.com>
2021-07-29 22:21:02 -0300

    test resol

--- js/cambiarres.js
+++ js/cambiarres.js
@@ -1,4 +1,5 @@
-if (document.body.clientWidth < 1400 && document.body.clientWidth > 1150 ) {
+if (document.body.clientWidth < 1400) {
     viewport = document.querySelector("meta[name=viewport]");
     viewport.setAttribute('content', 'width=device-width, initial-scale=0.67, user-scalable=0');
+    c
   }
