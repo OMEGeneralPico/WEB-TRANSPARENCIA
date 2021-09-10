db671bbe66e439294e75e6c8e596ec679210ed83
OMEGeneralPico <estadistica.gpico@gmail.com>
2021-09-10 10:09:23 -0300

    initial commit

---
+++ js/mapa/mapa1.js
@@ -0,0 +1,30 @@
+var myjson;
+
+$(document).ready(function () {
+  $.getJSON("json/mapa1.json", function (json) {
+    myjson = JSON.parse(JSON.stringify(json));
+  });
+
+  $("#mapitadiv").load("img/mapa_pico.svg", function () {
+    $("#mapita").click(function (evt) {
+      $("#info").html(
+        "<b style='font-weight: 700;'>Recaudación por servicios </b><br><br>"+
+        "Barrio: " +
+          myjson[evt.target.id].nombre +
+          "<br> Nivel de recaudación: " +
+          myjson[evt.target.id].porcentaje +
+          "<br> Cantidad facturada: " +
+          myjson[evt.target.id].cantFacturado +
+          "<br> Cantidad pagada: " +
+          myjson[evt.target.id].cantPagado
+      );
+
+      //$("#info").html("<table><tr><td>Barrio</td><td>"+myjson[evt.target.id].nombre + "</td></tr></table>"
+  
+          
+     // );
+
+      console.log(myjson[evt.target.id]);
+    });
+  });
+});
